b9984dfd0a967e3b0722e8a8a98ae26522107924
AnshulGarg2004 <ganshul2004@gmail.com>
2026-04-30 18:05:18 +0530

    ui setup

---
+++ Virtual-UI/lib/connectDB.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL!);
+        if(!conn) {
+            console.log("Cannot connect to db");
+        }
+
+        console.log("Mongodb connected successfully");
+        
+
+    } catch (error : any) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
+}
